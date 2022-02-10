6460fce6ddf672ed5c02504d087d00ca88fc1bc5
Remus <remuskwan23@gmail.com>
2022-02-10 18:35:49 +0800

    Added React Table Component
    - Includes search, filter and sort functionalities
    - Styled with TailwindUI

--- react-ui/src/app/containers/ManageProducts/index.js
+++ react-ui/src/app/containers/ManageProducts/index.js
@@ -1,34 +1,81 @@
+import { useMemo } from "react";
 import { ScaleIcon } from "@heroicons/react/outline";
-import {
-  CashIcon,
-  CheckCircleIcon,
-  ChevronRightIcon,
-  OfficeBuildingIcon,
-} from "@heroicons/react/solid";
-import MainWrapper from "../../components/MainWrapper";
-import { classNames } from "../../util/ClassNames";
+import { CheckCircleIcon, OfficeBuildingIcon } from "@heroicons/react/solid";
 
-const cards = [
-  { name: "Account balance", href: "#", icon: ScaleIcon, amount: "$30,659.45" },
-  // More items...
-];
-const transactions = [
-  {
-    id: 1,
-    name: "Payment to Molly Sanders",
-    href: "#",
-    amount: "$20,000",
-    currency: "USD",
-    status: "success",
-    date: "July 11, 2020",
-    datetime: "2020-07-11",
-  },
-  // More transactions...
-];
-const statusStyles = {
-  success: "bg-green-100 text-green-800",
-  processing: "bg-yellow-100 text-yellow-800",
-  failed: "bg-gray-100 text-gray-800",
+import MainWrapper from "../../components/MainWrapper";
+import { SimpleTable } from "../../components/Tables/SimpleTable";
+import { SelectColumnFilter } from "../../components/Tables/SimpleTable";
+
+const getData = () => {
+  const data = [
+    {
+      name: "Jane Cooper",
+      email: "jane.cooper@example.com",
+      title: "Regional Paradigm Technician",
+      department: "Optimization",
+      status: "Active",
+      role: "Admin",
+      imgUrl:
+        "https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=4&w=256&h=256&q=60",
+      age: 27,
+    },
+    {
+      name: "Cody Fisher",
+      email: "cody.fisher@example.com",
+      title: "Product Directives Officer",
+      department: "Intranet",
+      status: "Active",
+      role: "Owner",
+      imgUrl:
+        "https://images.unsplash.com/photo-1570295999919-56ceb5ecca61?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=4&w=256&h=256&q=60",
+      age: 43,
+    },
+    {
+      name: "Esther Howard",
+      email: "esther.howard@example.com",
+      title: "Forward Response Developer",
+      department: "Directives",
+      status: "Active",
+      role: "Member",
+      imgUrl:
+        "https://images.unsplash.com/photo-1520813792240-56fc4a3765a7?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=4&w=256&h=256&q=60",
+      age: 32,
+    },
+    {
+      name: "Jenny Wilson",
+      email: "jenny.wilson@example.com",
+      title: "Central Security Manager",
+      department: "Program",
+      status: "Active",
+      role: "Member",
+      imgUrl:
+        "https://images.unsplash.com/photo-1498551172505-8ee7ad69f235?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=4&w=256&h=256&q=60",
+      age: 29,
+    },
+    {
+      name: "Kristin Watson",
+      email: "kristin.watson@example.com",
+      title: "Lean Implementation Liaison",
+      department: "Mobility",
+      status: "Active",
+      role: "Admin",
+      imgUrl:
+        "https://images.unsplash.com/photo-1532417344469-368f9ae6d187?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=4&w=256&h=256&q=60",
+      age: 30,
+    },
+    {
+      name: "Cameron Williamson",
+      email: "cameron.williamson@example.com",
+      title: "Internal Applications Engineer",
+      department: "Security",
+      status: "Active",
+      role: "Member",
+      imgUrl:
+        "https://images.unsplash.com/photo-1566492031773-4f4e44671857?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=4&w=256&h=256&q=60",
+      age: 24,
+    },
+  ];
+  return [...data, ...data, ...data];
 };
 
 const header = () => (
@@ -36,13 +83,7 @@ const header = () => (
     <div className="px-4 sm:px-6 lg:max-w-6xl lg:mx-auto lg:px-8">
       <div className="py-6 md:flex md:items-center md:justify-between lg:border-t lg:border-gray-200">
         <div className="flex-1 min-w-0">
-          {/* Profile */}
           <div className="flex items-center">
-            <img
-              className="hidden h-16 w-16 rounded-full sm:block"
-              src="https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2.6&w=256&h=256&q=80"
-              alt=""
-            />
             <div>
               <div className="flex items-center">
                 <img
@@ -51,7 +92,7 @@ const header = () => (
                   alt=""
                 />
                 <h1 className="ml-3 text-2xl font-bold leading-7 text-gray-900 sm:leading-9 sm:truncate">
-                  Good morning, Emilia Birch
+                  Products
                 </h1>
               </div>
               <dl className="mt-6 flex flex-col sm:ml-3 sm:mt-1 sm:flex-row sm:flex-wrap">
@@ -94,226 +135,50 @@ const header = () => (
   </div>
 );
 
-const body = () => (
-  <div className="mt-8">
-    <div className="max-w-6xl mx-auto px-4 sm:px-6 lg:px-8">
-      <h2 className="text-lg leading-6 font-medium text-gray-900">Overview</h2>
-      <div className="mt-2 grid grid-cols-1 gap-5 sm:grid-cols-2 lg:grid-cols-3">
-        {/* Card */}
-        {cards.map((card) => (
-          <div
-            key={card.name}
-            className="bg-white overflow-hidden shadow rounded-lg"
-          >
-            <div className="p-5">
-              <div className="flex items-center">
-                <div className="flex-shrink-0">
-                  <card.icon
-                    className="h-6 w-6 text-gray-400"
-                    aria-hidden="true"
-                  />
-                </div>
-                <div className="ml-5 w-0 flex-1">
-                  <dl>
-                    <dt className="text-sm font-medium text-gray-500 truncate">
-                      {card.name}
-                    </dt>
-                    <dd>
-                      <div className="text-lg font-medium text-gray-900">
-                        {card.amount}
-                      </div>
-                    </dd>
-                  </dl>
-                </div>
-              </div>
-            </div>
-            <div className="bg-gray-50 px-5 py-3">
-              <div className="text-sm">
-                <a
-                  href={card.href}
-                  className="font-medium text-cyan-700 hover:text-cyan-900"
-                >
-                  View all
-                </a>
-              </div>
-            </div>
-          </div>
-        ))}
+const ManageProductsBody = () => {
+  const columns = useMemo(
+    () => [
+      {
+        Header: "Name",
+        accessor: "name",
+      },
+      {
+        Header: "Title",
+        accessor: "title",
+      },
+      {
+        Header: "Status",
+        accessor: "status",
+      },
+      {
+        Header: "Age",
+        accessor: "age",
+      },
+      {
+        Header: "Role",
+        accessor: "role",
+        Filter: SelectColumnFilter,
+        filter: "includes",
+      },
+    ],
+    []
+  );
+  const data = useMemo(() => getData(), []);
+  return (
+    <div className="max-w-6xl mx-auto px-4 sm:px-6 lg:px-8 pt-4">
+      <div className="mt-4">
+        <SimpleTable columns={columns} data={data} />
       </div>
     </div>
-
-    <h2 className="max-w-6xl mx-auto mt-8 px-4 text-lg leading-6 font-medium text-gray-900 sm:px-6 lg:px-8">
-      Recent activity
-    </h2>
-
-    {/* Activity list (smallest breakpoint only) */}
-    <div className="shadow sm:hidden">
-      <ul
-        role="list"
-        className="mt-2 divide-y divide-gray-200 overflow-hidden shadow sm:hidden"
-      >
-        {transactions.map((transaction) => (
-          <li key={transaction.id}>
-            <a
-              href={transaction.href}
-              className="block px-4 py-4 bg-white hover:bg-gray-50"
-            >
-              <span className="flex items-center space-x-4">
-                <span className="flex-1 flex space-x-2 truncate">
-                  <CashIcon
-                    className="flex-shrink-0 h-5 w-5 text-gray-400"
-                    aria-hidden="true"
-                  />
-                  <span className="flex flex-col text-gray-500 text-sm truncate">
-                    <span className="truncate">{transaction.name}</span>
-                    <span>
-                      <span className="text-gray-900 font-medium">
-                        {transaction.amount}
-                      </span>{" "}
-                      {transaction.currency}
-                    </span>
-                    <time dateTime={transaction.datetime}>
-                      {transaction.date}
-                    </time>
-                  </span>
-                </span>
-                <ChevronRightIcon
-                  className="flex-shrink-0 h-5 w-5 text-gray-400"
-                  aria-hidden="true"
-                />
-              </span>
-            </a>
-          </li>
-        ))}
-      </ul>
-
-      <nav
-        className="bg-white px-4 py-3 flex items-center justify-between border-t border-gray-200"
-        aria-label="Pagination"
-      >
-        <div className="flex-1 flex justify-between">
-          <a
-            href="#"
-            className="relative inline-flex items-center px-4 py-2 border border-gray-300 text-sm font-medium rounded-md text-gray-700 bg-white hover:text-gray-500"
-          >
-            Previous
-          </a>
-          <a
-            href="#"
-            className="ml-3 relative inline-flex items-center px-4 py-2 border border-gray-300 text-sm font-medium rounded-md text-gray-700 bg-white hover:text-gray-500"
-          >
-            Next
-          </a>
-        </div>
-      </nav>
-    </div>
-
-    {/* Activity table (small breakpoint and up) */}
-    <div className="hidden sm:block">
-      <div className="max-w-6xl mx-auto px-4 sm:px-6 lg:px-8">
-        <div className="flex flex-col mt-2">
-          <div className="align-middle min-w-full overflow-x-auto shadow overflow-hidden sm:rounded-lg">
-            <table className="min-w-full divide-y divide-gray-200">
-              <thead>
-                <tr>
-                  <th className="px-6 py-3 bg-gray-50 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
-                    Transaction
-                  </th>
-                  <th className="px-6 py-3 bg-gray-50 text-right text-xs font-medium text-gray-500 uppercase tracking-wider">
-                    Amount
-                  </th>
-                  <th className="hidden px-6 py-3 bg-gray-50 text-left text-xs font-medium text-gray-500 uppercase tracking-wider md:block">
-                    Status
-                  </th>
-                  <th className="px-6 py-3 bg-gray-50 text-right text-xs font-medium text-gray-500 uppercase tracking-wider">
-                    Date
-                  </th>
-                </tr>
-              </thead>
-              <tbody className="bg-white divide-y divide-gray-200">
-                {transactions.map((transaction) => (
-                  <tr key={transaction.id} className="bg-white">
-                    <td className="max-w-0 w-full px-6 py-4 whitespace-nowrap text-sm text-gray-900">
-                      <div className="flex">
-                        <a
-                          href={transaction.href}
-                          className="group inline-flex space-x-2 truncate text-sm"
-                        >
-                          <CashIcon
-                            className="flex-shrink-0 h-5 w-5 text-gray-400 group-hover:text-gray-500"
-                            aria-hidden="true"
-                          />
-                          <p className="text-gray-500 truncate group-hover:text-gray-900">
-                            {transaction.name}
-                          </p>
-                        </a>
-                      </div>
-                    </td>
-                    <td className="px-6 py-4 text-right whitespace-nowrap text-sm text-gray-500">
-                      <span className="text-gray-900 font-medium">
-                        {transaction.amount}{" "}
-                      </span>
-                      {transaction.currency}
-                    </td>
-                    <td className="hidden px-6 py-4 whitespace-nowrap text-sm text-gray-500 md:block">
-                      <span
-                        className={classNames(
-                          statusStyles[transaction.status],
-                          "inline-flex items-center px-2.5 py-0.5 rounded-full text-xs font-medium capitalize"
-                        )}
-                      >
-                        {transaction.status}
-                      </span>
-                    </td>
-                    <td className="px-6 py-4 text-right whitespace-nowrap text-sm text-gray-500">
-                      <time dateTime={transaction.datetime}>
-                        {transaction.date}
-                      </time>
-                    </td>
-                  </tr>
-                ))}
-              </tbody>
-            </table>
-            {/* Pagination */}
-            <nav
-              className="bg-white px-4 py-3 flex items-center justify-between border-t border-gray-200 sm:px-6"
-              aria-label="Pagination"
-            >
-              <div className="hidden sm:block">
-                <p className="text-sm text-gray-700">
-                  Showing <span className="font-medium">1</span> to{" "}
-                  <span className="font-medium">10</span> of{" "}
-                  <span className="font-medium">20</span> results
-                </p>
-              </div>
-              <div className="flex-1 flex justify-between sm:justify-end">
-                <a
-                  href="#"
-                  className="relative inline-flex items-center px-4 py-2 border border-gray-300 text-sm font-medium rounded-md text-gray-700 bg-white hover:bg-gray-50"
-                >
-                  Previous
-                </a>
-                <a
-                  href="#"
-                  className="ml-3 relative inline-flex items-center px-4 py-2 border border-gray-300 text-sm font-medium rounded-md text-gray-700 bg-white hover:bg-gray-50"
-                >
-                  Next
-                </a>
-              </div>
-            </nav>
-          </div>
-        </div>
-      </div>
-    </div>
-  </div>
-);
+  );
+};
 export default function ManageProducts() {
   return (
     <MainWrapper
       content={
         <>
           {header()}
-          {body()}
+          {<ManageProductsBody />}
         </>
       }
     />

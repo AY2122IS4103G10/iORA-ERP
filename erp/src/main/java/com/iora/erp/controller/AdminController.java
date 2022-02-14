5eecbe9f9f5aa39f50474c6bd54a862ed0b84cf5
Adeline <tan.adelinejy@gmail.com>
2022-02-15 02:25:19 +0800

    updated adminservice and impl to correct heading

--- erp/src/main/java/com/iora/erp/controller/AdminController.java
+++ erp/src/main/java/com/iora/erp/controller/AdminController.java
@@ -7,7 +7,7 @@ import com.iora.erp.enumeration.Country;
 import com.iora.erp.exception.EmployeeException;
 import com.iora.erp.model.company.Employee;
 import com.iora.erp.model.site.Site;
-import com.iora.erp.service.AdminService;
+import com.iora.erp.service.AdminServiceImpl;
 import com.iora.erp.service.EmployeeService;
 import com.iora.erp.service.SiteService;
 
@@ -32,7 +32,7 @@ public class AdminController {
     @Autowired
     private EmployeeService employeeService;
     @Autowired
-    private AdminService adminService;
+    private AdminServiceImpl adminService;
 
     // Employee/JobTitle/Department stuff here
 

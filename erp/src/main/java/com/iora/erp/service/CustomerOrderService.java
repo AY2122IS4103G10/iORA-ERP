402cc253461ebe98b881bbc4fc8a816794ca9a25
wongpengyu <72071732+wongpengyu@users.noreply.github.com>
2022-02-17 15:22:14 +0800

    update CustomerOrder service

--- erp/src/main/java/com/iora/erp/service/CustomerOrderService.java
+++ erp/src/main/java/com/iora/erp/service/CustomerOrderService.java
@@ -1,16 +1,28 @@
 package com.iora.erp.service;
 
+import java.time.LocalDate;
 import java.util.List;
 
 import com.iora.erp.exception.CustomerOrderException;
 import com.iora.erp.model.customerOrder.CustomerOrder;
+import com.iora.erp.model.customerOrder.CustomerOrderLI;
+import com.iora.erp.model.customerOrder.ExchangeLI;
+import com.iora.erp.model.customerOrder.Payment;
 import com.iora.erp.model.product.ProductItem;
 
 public interface CustomerOrderService {
     public abstract CustomerOrder getCustomerOrder(Long id) throws CustomerOrderException;
-    public abstract void createCustomerOrderLI(List<ProductItem> productItems);
-    public abstract void createPayment(double amount, String ccTransactionId);
-    public abstract void createCustomerOrder();
+    public abstract List<CustomerOrder> searchCustomerOrderByDate(LocalDate date);
+    public abstract void createCustomerOrder(CustomerOrder customerOrder);
+    public abstract void updateCustomerOrder(CustomerOrder customerOrder);
+    
+    public abstract CustomerOrderLI getCustomerOrderLI(Long id);
+    public abstract List<CustomerOrderLI> getAllCustomerOrderLIs();
+    public abstract void createCustomerOrderLI(CustomerOrderLI customerOrderLI);
 
+    public abstract Payment getPayment(Long id);
+    public abstract void createPayment(Payment payment);
+    public abstract List<Payment> getAllPayments();
 
+    public abstract void createExchangeLI(ExchangeLI exchangeLI);
 }

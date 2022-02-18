1236021a91a63cf48ca16e3c17c1737adc0776f6
delvenwpy <72071732+wongpengyu@users.noreply.github.com>
2022-02-18 16:04:36 +0800

    Update CustomerOrderService

--- erp/src/main/java/com/iora/erp/service/CustomerOrderService.java
+++ erp/src/main/java/com/iora/erp/service/CustomerOrderService.java
@@ -1,38 +1,39 @@
 package com.iora.erp.service;
 
-import java.time.LocalDate;
 import java.util.List;
 
 import com.iora.erp.exception.CustomerOrderException;
 import com.iora.erp.model.customerOrder.CustomerOrder;
 import com.iora.erp.model.customerOrder.CustomerOrderLI;
 import com.iora.erp.model.customerOrder.ExchangeLI;
+import com.iora.erp.model.customerOrder.OnlineOrder;
 import com.iora.erp.model.customerOrder.Payment;
 import com.iora.erp.model.customerOrder.RefundLI;
 
 public interface CustomerOrderService {
     public abstract CustomerOrder getCustomerOrder(Long id) throws CustomerOrderException;
-    public abstract List<CustomerOrder> searchCustomerOrderByDate(LocalDate date);
+    public abstract List<OnlineOrder> getOnlineOrders();
+    public abstract List<CustomerOrder> getInStoreOrders();
     public abstract void createCustomerOrder(CustomerOrder customerOrder);
-    public abstract void updateCustomerOrder(CustomerOrder customerOrder);
+    public abstract void updateCustomerOrder(CustomerOrder customerOrder) throws CustomerOrderException;
     
-    public abstract CustomerOrderLI getCustomerOrderLI(Long id);
-    public abstract List<CustomerOrderLI> getAllCustomerOrderLIs();
+    public abstract CustomerOrderLI getCustomerOrderLI(Long id) throws CustomerOrderException;
+    public abstract List<CustomerOrderLI> getCustomerOrderLIs(CustomerOrder customerOrder);
     public abstract void createCustomerOrderLI(CustomerOrderLI customerOrderLI);
-    public abstract void updateCustomerOrderLI(CustomerOrderLI customerOrderLI);
+    public abstract void updateCustomerOrderLI(CustomerOrderLI customerOrderLI) throws CustomerOrderException;
 
-    public abstract Payment getPayment(Long id);
+    public abstract Payment getPayment(Long id) throws CustomerOrderException;
     public abstract List<Payment> getAllPayments();
     public abstract void createPayment(Payment payment);
-    public abstract void updatePayment(Payment payment);
+    public abstract void updatePayment(Payment payment) throws CustomerOrderException;
 
-    public abstract void getExchangeLI(Long id);
+    public abstract ExchangeLI getExchangeLI(Long id) throws CustomerOrderException;
     public abstract List<ExchangeLI> getAllExchangeLIs();
     public abstract void createExchangeLI(ExchangeLI exchangeLI);
-    public abstract void updateExchangeLI(ExchangeLI exchangeLI);
+    public abstract void updateExchangeLI(ExchangeLI exchangeLI) throws CustomerOrderException;
 
-    public abstract void getRefundLI(Long id);
+    public abstract RefundLI getRefundLI(Long id) throws CustomerOrderException;
     public abstract List<RefundLI> getAllRefundLIs();
     public abstract void createRefundLI(RefundLI refundLI);
-    public abstract void updateRefundLI(RefundLI refundLI);
+    public abstract void updateRefundLI(RefundLI refundLI) throws CustomerOrderException;
 }
--- erp/src/main/java/com/iora/erp/service/CustomerOrderServiceImpl.java
+++ erp/src/main/java/com/iora/erp/service/CustomerOrderServiceImpl.java
@@ -1,135 +1,186 @@
 package com.iora.erp.service;
 
-import java.time.LocalDate;
 import java.util.List;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.TypedQuery;
 
 import com.iora.erp.exception.CustomerOrderException;
 import com.iora.erp.model.customerOrder.CustomerOrder;
 import com.iora.erp.model.customerOrder.CustomerOrderLI;
 import com.iora.erp.model.customerOrder.ExchangeLI;
+import com.iora.erp.model.customerOrder.OnlineOrder;
 import com.iora.erp.model.customerOrder.Payment;
 import com.iora.erp.model.customerOrder.RefundLI;
 
+    /*
+     * ---------------------------------------------------------
+     * Methods are not tested yet, for implementation in Second System Release
+     * ---------------------------------------------------------
+     */
+
 public class CustomerOrderServiceImpl implements CustomerOrderService {
+    @PersistenceContext
+    private EntityManager em;
 
     @Override
     public CustomerOrder getCustomerOrder(Long id) throws CustomerOrderException {
-        // TODO Auto-generated method stub
-        return null;
+        CustomerOrder customerOrder = em.find(CustomerOrder.class, id);
+
+        if (customerOrder == null) {
+            throw new CustomerOrderException("CustomerOrder with id " + id + " cannot be found.");
+        } else {
+            return customerOrder;
+        }
     }
 
     @Override
-    public List<CustomerOrder> searchCustomerOrderByDate(LocalDate date) {
-        // TODO Auto-generated method stub
-        return null;
+    public List<OnlineOrder> getOnlineOrders() {
+        TypedQuery<OnlineOrder> q = em.createQuery("SELECT oo FROM OnlineOrder oo", OnlineOrder.class);
+        return q.getResultList();
+    }
+
+    @Override
+    public List<CustomerOrder> getInStoreOrders() {
+        TypedQuery<CustomerOrder> q = em.createQuery("SELECT co FROM CustomerOrder co", CustomerOrder.class);
+        List<CustomerOrder> customerOrders = q.getResultList();
+
+        customerOrders.removeAll(getOnlineOrders());
+
+        return customerOrders;
     }
 
     @Override
     public void createCustomerOrder(CustomerOrder customerOrder) {
-        // TODO Auto-generated method stub
-        
+        em.persist(customerOrder);
     }
 
     @Override
-    public void updateCustomerOrder(CustomerOrder customerOrder) {
-        // TODO Auto-generated method stub
-        
+    public void updateCustomerOrder(CustomerOrder customerOrder) throws CustomerOrderException {
+        CustomerOrder old = getCustomerOrder(customerOrder.getId());
+        old.setDateTime(customerOrder.getDateTime());
+        old.setLineItems(customerOrder.getLineItems());
+        old.setPayments(customerOrder.getPayments());
+        old.setExhcangedLIs(customerOrder.getExhcangedLIs());
+        old.setRefundedLIs(customerOrder.getRefundedLIs());
     }
 
     @Override
-    public CustomerOrderLI getCustomerOrderLI(Long id) {
-        // TODO Auto-generated method stub
-        return null;
+    public CustomerOrderLI getCustomerOrderLI(Long id) throws CustomerOrderException {
+        CustomerOrderLI customerOrderLI = em.find(CustomerOrderLI.class, id);
+
+        if (customerOrderLI == null) {
+            throw new CustomerOrderException("CustomerOrder LineItem with id " + id + " cannot be found.");
+        } else {
+            return customerOrderLI;
+        }
     }
 
     @Override
-    public List<CustomerOrderLI> getAllCustomerOrderLIs() {
-        // TODO Auto-generated method stub
-        return null;
+    public List<CustomerOrderLI> getCustomerOrderLIs(CustomerOrder customerOrder) {
+        return customerOrder.getLineItems();
     }
 
     @Override
     public void createCustomerOrderLI(CustomerOrderLI customerOrderLI) {
-        // TODO Auto-generated method stub
-        
+        em.persist(customerOrderLI);
+
     }
 
     @Override
-    public void updateCustomerOrderLI(CustomerOrderLI customerOrderLI) {
-        // TODO Auto-generated method stub
-        
+    public void updateCustomerOrderLI(CustomerOrderLI customerOrderLI) throws CustomerOrderException {
+        CustomerOrderLI old = getCustomerOrderLI(customerOrderLI.getId());
+        old.setProductItems(customerOrderLI.getProductItems());
     }
 
     @Override
-    public Payment getPayment(Long id) {
-        // TODO Auto-generated method stub
-        return null;
+    public Payment getPayment(Long id) throws CustomerOrderException {
+        Payment payment = em.find(Payment.class, id);
+
+        if (payment == null) {
+            throw new CustomerOrderException("Payment with id " + id + " cannot be found.");
+        } else {
+            return payment;
+        }
     }
 
     @Override
     public List<Payment> getAllPayments() {
-        // TODO Auto-generated method stub
-        return null;
+        TypedQuery<Payment> q = em.createQuery("SELECT p FROM Payment p", Payment.class);
+        return q.getResultList();
     }
 
     @Override
     public void createPayment(Payment payment) {
-        // TODO Auto-generated method stub
-        
+        em.persist(payment);
+
     }
 
     @Override
-    public void updatePayment(Payment payment) {
-        // TODO Auto-generated method stub
-        
+    public void updatePayment(Payment payment) throws CustomerOrderException {
+        Payment old = getPayment(payment.getId());
+        old.setAmount(payment.getAmount());
+        old.setCcTransactionId(payment.getCcTransactionId());
+        old.setDateTime(payment.getDateTime());
     }
 
     @Override
-    public void getExchangeLI(Long id) {
-        // TODO Auto-generated method stub
-        
+    public ExchangeLI getExchangeLI(Long id) throws CustomerOrderException {
+        ExchangeLI exchangeLI = em.find(ExchangeLI.class, id);
+
+        if (exchangeLI == null) {
+            throw new CustomerOrderException("Echange Line Item with id " + id + " cannot be found.");
+        } else {
+            return exchangeLI;
+        }
     }
 
     @Override
     public List<ExchangeLI> getAllExchangeLIs() {
-        // TODO Auto-generated method stub
-        return null;
+        TypedQuery<ExchangeLI> q = em.createQuery("SELECT eLI FROM ExchangeLI eLI", ExchangeLI.class);
+        return q.getResultList();
     }
 
     @Override
     public void createExchangeLI(ExchangeLI exchangeLI) {
-        // TODO Auto-generated method stub
-        
+        em.persist(exchangeLI);
+
     }
 
     @Override
-    public void updateExchangeLI(ExchangeLI exchangeLI) {
-        // TODO Auto-generated method stub
-        
+    public void updateExchangeLI(ExchangeLI exchangeLI) throws CustomerOrderException {
+        ExchangeLI old = getExchangeLI(exchangeLI.getId());
+        old.setNewItem(exchangeLI.getNewItem());
+        old.setOldItem(exchangeLI.getOldItem());
     }
 
     @Override
-    public void getRefundLI(Long id) {
-        // TODO Auto-generated method stub
-        
+    public RefundLI getRefundLI(Long id) throws CustomerOrderException {
+        RefundLI refundLI = em.find(RefundLI.class, id);
+
+        if (refundLI == null) {
+            throw new CustomerOrderException("Refund Line Item with id " + id + " cannot be found.");
+        } else {
+            return refundLI;
+        }
     }
 
     @Override
     public List<RefundLI> getAllRefundLIs() {
-        // TODO Auto-generated method stub
-        return null;
+        TypedQuery<RefundLI> q = em.createQuery("SELECT rLI FROM RefundLI rLI", RefundLI.class);
+        return q.getResultList();
     }
 
     @Override
     public void createRefundLI(RefundLI refundLI) {
-        // TODO Auto-generated method stub
-        
+        em.persist(refundLI);
+
     }
 
     @Override
-    public void updateRefundLI(RefundLI refundLI) {
-        // TODO Auto-generated method stub
-        
+    public void updateRefundLI(RefundLI refundLI) throws CustomerOrderException {
+        RefundLI old = getRefundLI(refundLI.getId());
+        old.setRefundedItem(refundLI.getRefundedItem());
     }
-    
 }

9f56c2030122b617ba8a4942335c48c0df02a2d6
delvenwpy <72071732+wongpengyu@users.noreply.github.com>
2022-02-18 14:31:28 +0800

    Update CORS

--- erp/src/main/java/com/iora/erp/configuration/WebConfig.java
+++ erp/src/main/java/com/iora/erp/configuration/WebConfig.java
@@ -11,6 +11,8 @@ public class WebConfig implements WebMvcConfigurer {
 
 	@Override
 	public void addCorsMappings(CorsRegistry registry) {
-		registry.addMapping("/**");
+		registry.addMapping("/**")
+				.allowedOrigins("*")
+				.allowedMethods("GET", "PUT", "POST", "PATCH", "DELETE", "OPTIONS");
 	}
 }

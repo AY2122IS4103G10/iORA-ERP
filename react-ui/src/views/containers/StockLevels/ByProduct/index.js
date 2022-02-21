c58730718cd34b04da150a701ed0e08409dee894
Ruth Chong <ruth.cjn@gmail.com>
2022-02-22 00:57:42 +0800

    updated view products stock levels to include model name

--- react-ui/src/views/containers/StockLevels/ByProduct/index.js
+++ react-ui/src/views/containers/StockLevels/ByProduct/index.js
@@ -1,6 +1,6 @@
 import { useMem, useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { selectAllProductSL } from "../../../../stores/slices/productSlice";
+import { fetchProducts, selectAllProducts, selectAllProductSL } from "../../../../stores/slices/productSlice";
 import { getAllProductSL } from "../../../../stores/slices/productSlice";
 import { SelectableTable } from "../../../components/Tables/SelectableTable";
 import { SelectColumnFilter } from "../../../components/Tables/SelectableTable";
@@ -10,25 +10,32 @@ const columns = [
             Header: "SKU Code", 
             accessor: "sku"
         }, 
-        // {
-        //     Header: "Name", 
-        //     accessor: "name"
-        // },
+        {
+            Header: "Name", 
+            accessor: "name"
+        },
     ]
 
 
 export const ProductTable = () => {
     const dispatch = useDispatch();
-    const data = useSelector(selectAllProductSL);
+    const data = useSelector(selectAllProducts);
     const prodStatus = useSelector((state) => state.products.status);
+
+    const products = data.flatMap((model) => 
+      model.products.map((product) => ({
+        ...product,
+        name: model.name,
+      })));
+
     useEffect(() => {
-        prodStatus === "idle" && dispatch(getAllProductSL());
+        prodStatus === "idle" && dispatch(fetchProducts());
       }, [prodStatus, dispatch]);
 
     return (
         <div className="max-w-6xl mx-auto px-4 sm:px-6 lg:px-8 pt-4">
         <div className="mt-4">
-          <SelectableTable columns={columns} data={data} path="/sm/stocklevels/products" />
+          <SelectableTable columns={columns} data={products} path="/sm/stocklevels/products" />
         </div>
       </div>
     );

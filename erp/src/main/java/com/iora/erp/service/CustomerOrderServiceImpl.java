93ea93b77f4e8d2e36e4665ae316814adff36929
delvenwpy <72071732+wongpengyu@users.noreply.github.com>
2022-02-21 12:51:20 +0800

    update CustomerOrder

--- erp/src/main/java/com/iora/erp/service/CustomerOrderServiceImpl.java
+++ erp/src/main/java/com/iora/erp/service/CustomerOrderServiceImpl.java
@@ -21,12 +21,11 @@ import com.iora.erp.model.customerOrder.OnlineOrder;
 import com.iora.erp.model.customerOrder.Payment;
 import com.iora.erp.model.customerOrder.RefundLI;
 
-/*
- * ---------------------------------------------------------
- * Methods are not tested yet, for implementation in Second System Release
- * ---------------------------------------------------------
- */
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+@Service("customerOrderServiceImpl")
+@Transactional
 public class CustomerOrderServiceImpl implements CustomerOrderService {
     @PersistenceContext
     private EntityManager em;
@@ -43,19 +42,53 @@ public class CustomerOrderServiceImpl implements CustomerOrderService {
     }
 
     @Override
-    public List<OnlineOrder> getOnlineOrders() {
+    public List<OnlineOrder> getAllOnlineOrders() {
         TypedQuery<OnlineOrder> q = em.createQuery("SELECT oo FROM OnlineOrder oo", OnlineOrder.class);
         return q.getResultList();
     }
 
     @Override
-    public List<CustomerOrder> getInStoreOrders() {
+    public List<OnlineOrder> getOnlineOrdersBySite(Long siteId) {
+        TypedQuery<OnlineOrder> q = em.createQuery("SELECT oo FROM OnlineOrder oo WHERE oo.onlineStoreSiteId = :siteId",
+                OnlineOrder.class);
+        q.setParameter("siteId", siteId);
+        return q.getResultList();
+    }
+
+    @Override
+    public List<OnlineOrder> getOnlineOrdersBySiteDate(Long siteId, String date) {
+        TypedQuery<OnlineOrder> q = em.createQuery("SELECT oo FROM OnlineOrder oo WHERE oo.storeSiteId = :siteId AND SUBSTRING(oo.dateTime, 0, 10) = :date",
+        OnlineOrder.class);
+        q.setParameter("siteId", siteId);
+        q.setParameter("date", date);
+        return q.getResultList();
+    }
+
+    @Override
+    public List<CustomerOrder> getAllInStoreOrders() {
         TypedQuery<CustomerOrder> q = em.createQuery("SELECT co FROM CustomerOrder co", CustomerOrder.class);
         List<CustomerOrder> customerOrders = q.getResultList();
 
-        customerOrders.removeAll(getOnlineOrders());
+        customerOrders.removeAll(getAllOnlineOrders());
 
         return customerOrders;
+    }
+
+    @Override
+    public List<CustomerOrder> getInStoreOrdersBySite(Long siteId) {
+        TypedQuery<CustomerOrder> q = em.createQuery("SELECT co FROM CustomerOrder co WHERE co.storeSiteId = :siteId",
+                CustomerOrder.class);
+        q.setParameter("siteId", siteId);
+        return q.getResultList();
+    }
+
+    @Override
+    public List<CustomerOrder> getInStoreOrdersBySiteDate(Long siteId, String date) {
+        TypedQuery<CustomerOrder> q = em.createQuery("SELECT co FROM CustomerOrder co WHERE co.storeSiteId = :siteId AND SUBSTRING(co.dateTime, 0, 10) = :date",
+                CustomerOrder.class);
+        q.setParameter("siteId", siteId);
+        q.setParameter("date", date);
+        return q.getResultList();
     }
 
     @Override
@@ -206,7 +239,6 @@ public class CustomerOrderServiceImpl implements CustomerOrderService {
                 .stream()
                 .mapToDouble(x -> x.getAmount())
                 .sum();
-
         List<MembershipTier> tiers = em
                 .createQuery("SELECT m FROM MembershipTier m ORDER BY m.multiplier ASC", MembershipTier.class)
                 .getResultList();
@@ -226,10 +258,12 @@ public class CustomerOrderServiceImpl implements CustomerOrderService {
         } else {
             Integer membershipPoints = customer.getMembershipPoints();
             for (Payment p : order.getPayments()) {
-                membershipPoints = Integer.sum(membershipPoints, (int) (p.getAmount() * membershipTier.getMultiplier()));
+                membershipPoints = Integer.sum(membershipPoints,
+                        (int) (p.getAmount() * membershipTier.getMultiplier()));
             }
             customer.setMembershipPoints(membershipPoints);
         }
 
     }
+
 }

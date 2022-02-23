b7079fc0486fc58d40ef9dac9d664066a1cd7f25
wongpengyu <72071732+wongpengyu@users.noreply.github.com>
2022-02-23 11:12:22 +0800

    update Product Loader
    Reduce JSON file to 2/9 of the original, load time will be less than 10 seconds now

--- erp/src/main/java/com/iora/erp/data/DataLoader.java
+++ erp/src/main/java/com/iora/erp/data/DataLoader.java
@@ -306,23 +306,23 @@ public class DataLoader implements CommandLineRunner {
         //                 System.out.println(ex.getMessage());
         //         }
 
-                // Add many productItems of the product
-                try {
-                        productService.createProductItem("T1ZZ3OA60NOBK18H", "ASK0009968A-1");
-                        productService.createProductItem("NJCTRE9HI281F8B7", "ASK0009968A-2");
-                        productService.createProductItem("1HAC5IJD2Y8R2X4G", "ASK0009968A-3");
-                        productService.createProductItem("ANGA64O891NH0WC4", "ASK0009968A-4");
-                        productService.createProductItem("RBKD1XUUTN8ZVO3Z", "ASK0009968A-5");
-                        productService.createProductItem("T5VWY8V1V8QQUVG1", "ASK0009968A-6");
-                        productService.createProductItem("WQ8ED2RQ83VV1HO2", "ASK0009968A-7");
-                        productService.createProductItem("HAPPYM46XOVU766P", "ASK0009968A-8");
-                        productService.createProductItem("1V76V4FAM2NX90IU", "ASK0009968A-9");
-                        productService.createProductItem("1Z3G165G9DMDGQMD", "ASK0009968A-10");
-                        productService.createProductItem("6AND0X6G25KLNVUE", "ASK0009968A-11");
-                        productService.createProductItem("OZ9O75UATQ8P8QS4", "ASK0009968A-12");
-                } catch (ProductItemException e) {
-                        e.printStackTrace();
-                }
+                // // Add many productItems of the product
+                // try {
+                //         productService.createProductItem("T1ZZ3OA60NOBK18H", "ASK0009968A-1");
+                //         productService.createProductItem("NJCTRE9HI281F8B7", "ASK0009968A-2");
+                //         productService.createProductItem("1HAC5IJD2Y8R2X4G", "ASK0009968A-3");
+                //         productService.createProductItem("ANGA64O891NH0WC4", "ASK0009968A-4");
+                //         productService.createProductItem("RBKD1XUUTN8ZVO3Z", "ASK0009968A-5");
+                //         productService.createProductItem("T5VWY8V1V8QQUVG1", "ASK0009968A-6");
+                //         productService.createProductItem("WQ8ED2RQ83VV1HO2", "ASK0009968A-7");
+                //         productService.createProductItem("HAPPYM46XOVU766P", "ASK0009968A-8");
+                //         productService.createProductItem("1V76V4FAM2NX90IU", "ASK0009968A-9");
+                //         productService.createProductItem("1Z3G165G9DMDGQMD", "ASK0009968A-10");
+                //         productService.createProductItem("6AND0X6G25KLNVUE", "ASK0009968A-11");
+                //         productService.createProductItem("OZ9O75UATQ8P8QS4", "ASK0009968A-12");
+                // } catch (ProductItemException e) {
+                //         e.printStackTrace();
+                // }
 
                 // Generate 10 $10 vouchers
                 customerService.generateVouchers(10, 10, "2022-02-16");

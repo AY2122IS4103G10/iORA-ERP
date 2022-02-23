44252ab0143ad18f92ed24c1bfaeb0e81abe43d1
Adeline <tan.adelinejy@gmail.com>
2022-02-23 21:52:59 +0800

    updated on countries and login

--- erp/src/main/java/com/iora/erp/controller/AuthenticationController.java
+++ erp/src/main/java/com/iora/erp/controller/AuthenticationController.java
@@ -1,6 +1,8 @@
 package com.iora.erp.controller;
 
 import com.iora.erp.model.company.Employee;
+import com.iora.erp.model.customer.Customer;
+import com.iora.erp.service.CustomerService;
 import com.iora.erp.service.EmployeeService;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -15,12 +17,23 @@ public class AuthenticationController {
 
     @Autowired
     private EmployeeService employeeService;
+    @Autowired
+    private CustomerService customerService;
     
 
     @GetMapping(path = "/empLogin", produces = "application/json")
     public Employee employeeLogin(@RequestParam String username, @RequestParam String password) {
         try {
             return employeeService.loginAuthentication(username, password);
+        } catch (Exception e) {
+            return null;
+        }
+    }
+
+    @GetMapping(path = "/login", produces = "application/json")
+    public Customer customerLogin(@RequestParam String email, @RequestParam String password) {
+        try {
+            return customerService.loginAuthentication(email, password);
         } catch (Exception e) {
             return null;
         }

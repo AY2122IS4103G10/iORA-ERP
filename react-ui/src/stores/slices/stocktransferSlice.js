4c6b2d6629d0ebb0fd20f3f1922b27626f5e815f
Ruth Chong <ruth.cjn@gmail.com>
2022-02-26 02:44:51 +0800

    stock transfer slice

---
+++ react-ui/src/stores/slices/stocktransferSlice.js
@@ -0,0 +1,48 @@
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+import { api, sitesApi } from "../../environments/Api"
+
+
+
+const initialState = {
+    orders: [],
+    currOrder: {}, 
+    status: "idle", 
+    error: null
+}
+
+export const getAllStockTransfer = createAsyncThunk(
+    "stocktransfer/getAllOrders",
+    async () => {
+        const response = await api.getAll("/sam/stockTransferOrder/all");
+        return response.data;
+    }
+)
+
+export const createStockTransfer = createAsyncThunk(
+    "stocktransfer/create",
+    async (order, siteId) => {
+        const response = await api.create(`sam/stockTransferOrder/create/1`, order)
+        return response.data;
+    }
+)
+
+
+const stocktransferSlice = createSlice({
+    name: "stocktransfer",
+    initialState,
+    extraReducers(builder) {
+        builder.addCase(createStockTransfer.pending, (state, action) => {
+            state.status = "loading";
+          });
+        builder.addCase(createStockTransfer.fulfilled, (state, action) => {
+        state.status = "succeeded";
+        });
+        builder.addCase(createStockTransfer.rejected, (state, action) => {
+        state.status = "failed";
+        });
+    }
+})
+
+export default stocktransferSlice.reducer;
+
+export const selectAllOrders = (state) => state.stocktransfer.orders;

8eb3a058351f03ffa2f7f1c3dfdf268b14baf217
Adeline <tan.adelinejy@gmail.com>
2022-02-28 02:24:25 +0800

    debugged and formatted template - order purchase

--- react-ui/src/stores/slices/posSlice.js
+++ react-ui/src/stores/slices/posSlice.js
@@ -1,13 +1,36 @@
 import {createAsyncThunk, createSlice} from "@reduxjs/toolkit";
-import {api} from "../../environments/Api";
+import {api, posApi} from "../../environments/Api";
 
 const initialState = {
-  order: [],
+  pos: [],
   status: "idle",
   error: null,
 };
 
-export const fetchSiteOrders = createAsyncThunk("pos/main", async () => {
-  const response = await api.getAll(`/store/customerOrder/`);
+export const fetchSiteOrders = createAsyncThunk("str/pos", async (siteId) => {
+  const response = await posApi.getOrders(`store/customerOrder/${siteId}`);
   return response.data;
 });
+
+const posSlice = createSlice({
+  name: "pos",
+  initialState,
+
+  extraReducers(builder) {
+    builder.addCase(fetchSiteOrders.pending, (state, action) => {
+      state.status = "loading";
+    });
+    builder.addCase(fetchSiteOrders.fulfilled, (state, action) => {
+      state.status = "succeeded";
+      state.posOrders = action.payload;
+    });
+    builder.addCase(fetchSiteOrders.rejected, (state, action) => {
+      state.status = "loading";
+      state.error = action.error.message;
+    });
+  },
+});
+
+export default posSlice.reducer;
+
+export const selectAllOrder = (state) => state.pos.posOrders;
--- react-ui/src/views/containers/POS/Main/index.js
+++ react-ui/src/views/containers/POS/Main/index.js
@@ -1,7 +1,11 @@
-import {Link, useLocation, useMatch} from "react-router-dom";
-import {useState, useSelector, useEffect, useMemo} from "react";
+import {Link} from "react-router-dom";
+import {useState, useEffect, useMemo} from "react";
 import {posApi} from "../../../../environments/Api";
+import {useSelector, useDispatch} from "react-redux";
 import {EditableCell, SimpleTable} from "../../../components/Tables/SimpleTable";
+import {selectAllOrder} from "../../../../stores/slices/posSlice";
+import {fetchSiteOrders} from "../../../../stores/slices/posSlice";
+import {Header} from "../../../components/Header";
 
 const exitButton = () => {
   return (
@@ -14,100 +18,30 @@ const exitButton = () => {
   );
 };
 
-const header = () => {
-  return (
-    <div className="px-4 sm:px-6 lg:max-w-5/6 lg:mx-auto lg:px-8 flex">
-      <div className="w-1/6 pt-4 md:flex md:items-center md:justify-between lg:border-t lg:border-gray-200"></div>
-      <div className="w-1/6 pt-4 md:flex md:items-center md:justify-between lg:border-t lg:border-gray-200">
-        <div className="py-4 md:flex flex items-left justify-between border-b border-indigo-500 lg:border-none">
-          <h1 className="ml-3 md:flex text-2xl font-bold leading-7 text-gray-900 sm:leading-9 sm:truncate">
-            POS System
-          </h1>
-        </div>
-      </div>
-      <div className="w-3/6 py-8 pb-5 border-b border-gray-200 sm:pb-0 ">
-        <ul className="flex flex-wrap -mb-px">
-          <li className="mr-2">
-            <a
-              href="/pos/order"
-              className="inline-block py-4 px-4 text-sm font-medium text-center text-gray-500 rounded-t-lg border-b-2 border-transparent hover:text-gray-600 hover:border-gray-300 dark:text-gray-400 dark:hover:text-gray-300">
-              Purchase
-            </a>
-          </li>
-          <li className="mr-2">
-            <a
-              href="#"
-              className="inline-block py-4 px-4 text-sm font-medium text-center text-blue-600 rounded-t-lg border-b-2 border-blue-600 active dark:text-blue-500 dark:border-blue-500"
-              aria-current="page">
-              Purchase History
-            </a>
-          </li>
-          <li className="mr-2">
-            <a
-              href="#"
-              className="inline-block py-4 px-4 text-sm font-medium text-center text-gray-500 rounded-t-lg border-b-2 border-transparent hover:text-gray-600 hover:border-gray-300 dark:text-gray-400 dark:hover:text-gray-300">
-              Store Pick-up
-            </a>
-          </li>
-          <li className="mr-2">
-            <a
-              href="#"
-              className="inline-block py-4 px-4 text-sm font-medium text-center text-gray-500 rounded-t-lg border-b-2 border-transparent hover:text-gray-600 hover:border-gray-300 dark:text-gray-400 dark:hover:text-gray-300">
-              Administrative
-            </a>
-          </li>
-        </ul>
-      </div>
-      <div className="ml-12 py-6">
-        <Link to={`#`}>
-          <button
-            type="button"
-            className="ml-10 inline-flex items-center px-4 py-2 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-indigo-700 hover:bg-indigo-400 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
-            Logout
-          </button>
-        </Link>
-      </div>
-    </div>
-  );
-};
-
 export const OrderTable = ({data}) => {
-  //const [site, setSite] = u
-  /*const columns = useMemo(
+  const columns = useMemo(
     () => [
       {
         Header: "OrderId",
-        accessor: "",
+        accessor: "id",
       },
       {
-        Header: "Name",
-        accessor: "name",
+        Header: "DateTime",
+        accessor: "dateTime",
       },
       {
-        Header: "Company",
-        accessor: (row) => row.company.name,
-        Filter: SelectColumnFilter,
-        filter: "includes",
+        Header: "Amount",
+        accessor: (row) => row.payments.name.amount,
       },
       {
-        Header: "Country",
-        accessor: (row) => row.address.country,
-        Filter: SelectColumnFilter,
-        filter: "includes",
+        Header: "Customer Number",
+        accessor: (row) => row.customer.id,
       },
     ],
     []
-  );*/
+  );
 };
-const orderTable = (siteId) => {
-  /*const data = useSelector(fetchSiteOrders);
-  const posStatus = useSelector((state) => state.pos.status);
-  const error = useSelector((state) => state.site.error);
-
-  useEffect(() => {
-    procurementsStatus === "idle" && dispatch(fetchProcurements());
-  }, [procurementsStatus, dispatch]);*/
-
+const orderTable = (data) => {
   return (
     <div className="flex flex-col">
       <div className="-my-2 overflow-x-auto sm:-mx-4 lg:-mx-6">
@@ -134,7 +68,7 @@ const orderTable = (siteId) => {
                   <th
                     scope="col"
                     className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
-                    CustomerNumber
+                    Customer Number
                   </th>
                   <th scope="col" className="relative px-6 py-3">
                     <span className="sr-only"></span>
@@ -168,22 +102,24 @@ const orderTable = (siteId) => {
   );
 };
 
-export const PosMain = (site) => {
-  const siteId = useState(site);
-  const [order, setOrder] = useState([]);
+const tabs = [
+  {name: "By Site", href: "sites", current: true},
+  {name: "By Products", href: "products", current: false},
+];
+
+export const PosMain = () => {
+  const dispatch = useDispatch();
+  const data = useSelector(selectAllOrder);
+  const orderStatus = useSelector((state) => state.pos.status);
+  const siteId = useState(1);
 
   useEffect(() => {
-    posApi.getOrders("/store/customerOrder/", siteId).then((response) => {
-      setOrder(response.data);
-    });
-  }, [siteId]);
+    orderStatus === "idle" && dispatch(fetchSiteOrders(siteId));
+  }, [orderStatus, dispatch]);
 
   return (
     <>
-      <div className="bg-white shadow">{header()}</div>
-      <div class="w-10/12 py-10 md:container md:mx-auto">
-        <OrderTable data={order} />
-      </div>
+      <Header title="Order Purchase" />
     </>
   );
 };

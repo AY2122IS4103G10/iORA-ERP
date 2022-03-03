235691ebd04774130dc9bcc733ebdafa6ee638a6
Remus <remuskwan23@gmail.com>
2022-03-03 18:20:41 +0800

    Minor visual update

--- react-ui/src/views/containers/Auth/Home/index.js
+++ react-ui/src/views/containers/Auth/Home/index.js
@@ -18,8 +18,16 @@ function classNames(...classes) {
   return classes.filter(Boolean).join(" ");
 }
 
-export const EnterStoreModal = ({ open, closeModal, stores, store, setStore, siteCode, setSiteCode, handleEnterStore }) => {
-
+export const EnterStoreModal = ({
+  open,
+  closeModal,
+  stores,
+  store,
+  setStore,
+  siteCode,
+  setSiteCode,
+  handleEnterStore,
+}) => {
   return (
     <SimpleModal open={open} closeModal={closeModal}>
       <div className="inline-block align-bottom bg-white h-80 rounded-lg px-4 pt-5 pb-4 text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:align-middle sm:max-w-sm sm:w-full sm:p-6">
@@ -37,7 +45,8 @@ export const EnterStoreModal = ({ open, closeModal, stores, store, setStore, sit
               options={stores}
               selected={store}
               setSelected={setStore}
-            /></div>
+            />
+          </div>
           <input
             type="email"
             name="email"
@@ -46,19 +55,19 @@ export const EnterStoreModal = ({ open, closeModal, stores, store, setStore, sit
             placeholder="Site Code"
             value={siteCode}
             onChange={setSiteCode}
-            onKeyPress={(e) => e.key === 'Enter' && handleEnterStore()}
+            onKeyPress={(e) => e.key === "Enter" && handleEnterStore()}
           />
         </div>
         <div className="pt-4">
           <div className="mt-5 grid grid-cols-1 gap-2 justify-center">
-          <button
+            <button
               type="submit"
               className="inline-flex justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-cyan-600 hover:bg-cyan-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-cyan-500"
               onClick={handleEnterStore}
             >
               Enter
             </button>
-            
+
             <button
               type="button"
               className="bg-white py-2 px-4 border border-gray-300 rounded-md shadow-sm text-sm font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-cyan-500"
@@ -71,8 +80,7 @@ export const EnterStoreModal = ({ open, closeModal, stores, store, setStore, sit
       </div>
     </SimpleModal>
   );
-
-}
+};
 
 const Header = ({ name, jobTitle, stats }) => {
   return (
@@ -86,14 +94,14 @@ const Header = ({ name, jobTitle, stats }) => {
             <div className="flex-shrink-0">
               <img
                 className="mx-auto h-20 w-20 rounded-full"
-                src={"https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"}
+                src={
+                  "https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
+                }
                 alt=""
               />
             </div>
             <div className="mt-4 text-center sm:mt-0 sm:pt-1 sm:text-left">
-              <p className="text-sm font-medium text-gray-600">
-                Welcome back,
-              </p>
+              <p className="text-sm font-medium text-gray-600">Welcome back,</p>
               <p className="text-xl font-bold text-gray-900 sm:text-2xl">
                 {name}
               </p>
@@ -116,14 +124,15 @@ const Header = ({ name, jobTitle, stats }) => {
             key={stat.label}
             className="px-6 py-5 text-sm font-medium text-center"
           >
-            <span className="text-gray-900">{stat.label}</span>{": "}
+            <span className="text-gray-900">{stat.label}</span>
+            {": "}
             <span className="text-gray-600">{stat.value}</span>
           </div>
         ))}
       </div>
     </div>
   );
-}
+};
 
 const Tiles = ({ actions, openEnterStoreModal }) => {
   return (
@@ -140,7 +149,8 @@ const Tiles = ({ actions, openEnterStoreModal }) => {
             actionIdx === actions.length - 1
               ? "rounded-bl-lg rounded-br-lg sm:rounded-bl-none"
               : "",
-            "relative group bg-white p-6 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-500 m-4 "
+            action.focus,
+            "relative group bg-white p-6 focus-within:ring-2 focus-within:ring-inset m-4 "
           )}
         >
           <div>
@@ -156,18 +166,22 @@ const Tiles = ({ actions, openEnterStoreModal }) => {
           </div>
           <div className="mt-8">
             <h3 className="text-lg font-medium">
-              {action.href === "/str" ?
-                <button className="focus:outline-none" onClick={() => openEnterStoreModal()}>
+              {action.href === "/str" ? (
+                <button
+                  className="focus:outline-none"
+                  onClick={() => openEnterStoreModal()}
+                >
                   {/* Extend touch target to entire panel */}
                   <span className="absolute inset-0" aria-hidden="true" />
                   <strong>{action.title}</strong>
                 </button>
-                : <a href={action.href} className="focus:outline-none">
+              ) : (
+                <a href={action.href} className="focus:outline-none">
                   {/* Extend touch target to entire panel */}
                   <span className="absolute inset-0" aria-hidden="true" />
                   <strong>{action.title}</strong>
                 </a>
-              }
+              )}
             </h3>
             <p className="mt-2 text-sm text-gray-500">{action.description}</p>
           </div>
@@ -187,14 +201,15 @@ const Tiles = ({ actions, openEnterStoreModal }) => {
         </div>
       ))}
     </div>
-  )
-}
+  );
+};
 
 const paths = [
   {
     title: "Sales and Marketing",
     href: "/sm",
     icon: UsersIcon,
+    focus: "focus-within:ring-indigo-500",
     iconForeground: "text-indigo-700",
     iconBackground: "bg-indigo-50",
     description:
@@ -204,6 +219,7 @@ const paths = [
     title: "Administrator",
     href: "/ad",
     icon: FolderOpenIcon,
+    focus: "focus-within:ring-pink-500",
     iconForeground: "text-pink-700",
     iconBackground: "bg-pink-50",
     description:
@@ -213,6 +229,7 @@ const paths = [
     title: "Manufacturing",
     href: "/mf",
     icon: CogIcon,
+    focus: "focus-within:ring-yellow-500",
     iconForeground: "text-yellow-700",
     iconBackground: "bg-yellow-50",
     description: "Assist procurement orders and view all updates.",
@@ -221,6 +238,7 @@ const paths = [
     title: "Warehouse",
     href: "/wh",
     icon: TruckIcon,
+    focus: "focus-within:ring-teal-500",
     iconForeground: "text-teal-700",
     iconBackground: "bg-teal-50",
     description:
@@ -230,6 +248,7 @@ const paths = [
     title: "Store",
     href: "/str",
     icon: ShoppingBagIcon,
+    focus: "focus-within:ring-rose-500",
     iconForeground: "text-rose-700",
     iconBackground: "bg-rose-50",
     description:
@@ -242,34 +261,38 @@ export function Home() {
   const { addToast } = useToasts();
   const [openEnterStore, setOpenEnterStore] = useState(false);
   const [siteCode, setSiteCode] = useState("");
-  const [storeNames, setStoreNames] = useState({ 0 : "Choose one" });
-  const stores = Object.keys(storeNames).map((key) => { return { id: key, name: storeNames[key] } });
+  const [storeNames, setStoreNames] = useState({ 0: "Choose one" });
+  const stores = Object.keys(storeNames).map((key) => {
+    return { id: key, name: storeNames[key] };
+  });
   const [store, setStore] = useState(stores[0]);
 
   useEffect(() => {
     localStorage.removeItem("siteId");
     api.getAll("/store/storeNames").then((response) => {
-      setStoreNames({0 : "Choose one", ...response.data});
-    })
+      setStoreNames({ 0: "Choose one", ...response.data });
+    });
   }, [setStoreNames]);
 
   const handleEnterStore = () => {
-    api.getAll(`/store/storeLogin?id=${store.id}&siteCode=${siteCode}`)
-    .then((response) => {
-      localStorage.setItem("siteId", response.data.id);
-      addToast(`Successfully logged in to ${store.name}`, {
-        appearance: "success",
-        autoDismiss: true,
+    api
+      .getAll(`/store/storeLogin?id=${store.id}&siteCode=${siteCode}`)
+      .then((response) => {
+        localStorage.setItem("siteId", response.data.id);
+        addToast(`Successfully logged in to ${store.name}`, {
+          appearance: "success",
+          autoDismiss: true,
+        });
+        closeEnterStoreModal();
+        navigate("/str");
+      })
+      .catch((err) => {
+        addToast(`Error: ${err.message}`, {
+          appearance: "error",
+          autoDismiss: true,
+        });
       });
-      closeEnterStoreModal();
-      navigate("/str");
-    }).catch((err) => {
-      addToast(`Error: ${err.message}`, {
-        appearance: "error",
-        autoDismiss: true,
-      });
-    })
-  }
+  };
 
   const openEnterStoreModal = () => setOpenEnterStore(true);
   const closeEnterStoreModal = () => setOpenEnterStore(false);
@@ -277,14 +300,28 @@ export function Home() {
   const stats = [
     { label: "Company", value: user.company.name },
     { label: "Department", value: user.department.deptName },
-    { label: `${user.payType.slice(0, 1)}${user.payType.slice(1).toLowerCase()} Salary`, value: user.salary },
+    {
+      label: `${user.payType.slice(0, 1)}${user.payType
+        .slice(1)
+        .toLowerCase()} Salary`,
+      value: `$${user.salary.toFixed(2)}`,
+    },
   ];
-  const availablePaths = accessRightsMap(user.jobTitle.responsibility).map(x => paths[x]);
+  const availablePaths = accessRightsMap(user.jobTitle.responsibility).map(
+    (x) => paths[x]
+  );
 
   return (
     <>
-      <Header jobTitle={user.jobTitle.description} name={user.name} stats={stats} />
-      <Tiles actions={availablePaths} openEnterStoreModal={openEnterStoreModal} />
+      <Header
+        jobTitle={user.jobTitle.description}
+        name={user.name}
+        stats={stats}
+      />
+      <Tiles
+        actions={availablePaths}
+        openEnterStoreModal={openEnterStoreModal}
+      />
       <EnterStoreModal
         open={openEnterStore}
         closeModal={closeEnterStoreModal}
@@ -293,7 +330,8 @@ export function Home() {
         setStore={(e) => setStore(e)}
         siteCode={siteCode}
         setSiteCode={(e) => setSiteCode(e.target.value)}
-        handleEnterStore={handleEnterStore} />
+        handleEnterStore={handleEnterStore}
+      />
     </>
-  )
+  );
 }

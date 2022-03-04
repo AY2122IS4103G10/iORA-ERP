ae52346b7b89a71e39ad9f1570560acb4c8f8962
Remus <remuskwan23@gmail.com>
2022-03-04 18:19:32 +0800

    Fixed Company Delete

--- react-ui/src/views/containers/Companies/CompanyDetails/index.js
+++ react-ui/src/views/containers/Companies/CompanyDetails/index.js
@@ -180,7 +180,7 @@ export const CompanyDetails = () => {
           <Header
             company={company}
             openModal={openModal}
-            available={company.available}
+            available={company.active}
           />
           <div className="mt-8 max-w-3xl mx-auto grid grid-cols-1 gap-6 sm:px-6 lg:max-w-7xl lg:grid-flow-col-dense lg:grid-cols-1">
             <div className="space-y-6 lg:col-start-1 lg:col-span-2">

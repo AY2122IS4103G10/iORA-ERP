ed5c934f4872aea6984323052974134e638adb75
Remus <remuskwan23@gmail.com>
2022-03-04 16:14:13 +0800

    Visual change to Voucher Details

--- react-ui/src/views/containers/Vouchers/VoucherDetails/index.js
+++ react-ui/src/views/containers/Vouchers/VoucherDetails/index.js
@@ -61,7 +61,7 @@ const VoucherDetailsBody = ({
               <button
                 type="button"
                 className={classNames(
-                  !redeemed ? "bg-white hover:bg-gray-50" : "bg-gray-200",
+                  !redeemed && issued ? "bg-white hover:bg-gray-50" : "bg-gray-200",
                   "inline-flex justify-center px-4 py-2 border border-gray-300 shadow-sm text-sm font-medium rounded-md text-gray-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-cyan-500"
                 )}
                 onClick={onRedeemClicked}

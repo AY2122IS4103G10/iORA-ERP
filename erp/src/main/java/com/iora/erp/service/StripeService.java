3f9037a045851169219f690b8cfec1543adb7ba2
delvenwpy <72071732+wongpengyu@users.noreply.github.com>
2022-03-16 10:45:27 +0800

    StripeService: Cancel & Refund

--- erp/src/main/java/com/iora/erp/service/StripeService.java
+++ erp/src/main/java/com/iora/erp/service/StripeService.java
@@ -4,7 +4,11 @@ import java.util.List;
 
 import com.iora.erp.model.customerOrder.CustomerOrderLI;
 import com.stripe.exception.StripeException;
+import com.stripe.model.PaymentIntent;
+import com.stripe.model.Refund;
 
 public interface StripeService {
     public abstract String chargeCreditCard(List<CustomerOrderLI> lineItems) throws StripeException;
+    public abstract PaymentIntent cancelPayment(String clientSecret) throws StripeException;
+    public abstract Refund refundPayment(String clientSecret, double amount) throws StripeException;
 }

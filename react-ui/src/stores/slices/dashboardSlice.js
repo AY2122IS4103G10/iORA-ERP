1e1ecd62c5486563219434016420e215e6476d35
hongpeigoh <57928283+hongpeigoh@users.noreply.github.com>
2022-03-31 00:50:26 +0800

    dashboard store

---
+++ react-ui/src/stores/slices/dashboardSlice.js
@@ -0,0 +1,37 @@
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+import { dashboardApi, sitesApi } from "../../environments/Api";
+
+const initialState = {
+  stockLevelSites: [],
+  stockLevelProducts: [],
+  status: "idle",
+  error: null,
+};
+
+const dashboardSlice = createSlice({
+  name: "customers",
+  initialState,
+  extraReducers(builder) {
+    builder.addCase(getStockLevelSites.pending, (state, action) => {
+      state.status = "loading";
+    });
+    builder.addCase(getStockLevelSites.fulfilled, (state, action) => {
+      state.status = "succeeded";
+      state.stockLevelSites = action.payload;
+    });
+    builder.addCase(getStockLevelSites.rejected, (state, action) => {
+      state.status = "failed";
+      state.error = action.error.message;
+    });
+  },
+});
+
+export default dashboardSlice.reducer;
+
+export const getStockLevelSites = createAsyncThunk(
+  "dashboard/getStockLevelSites",
+  async () => {
+    const response = await sitesApi.getAll();
+    return response.data;
+  }
+);

a06c27b454d7d655d16b0520ccbdff4739bf021e
hongpeigoh <57928283+hongpeigoh@users.noreply.github.com>
2022-03-30 17:31:19 +0800

    analytics page very cool (pls npm i)

--- react-ui/src/views/containers/Dashboard/ManageDashboard/index.js
+++ react-ui/src/views/containers/Dashboard/ManageDashboard/index.js
@@ -1,8 +1,28 @@
+import {
+  ArcElement,
+  CategoryScale,
+  Chart as ChartJS,
+  Legend,
+  LinearScale,
+  LineElement,
+  PointElement,
+  Title,
+  Tooltip
+} from "chart.js";
+import { Doughnut, Line } from "react-chartjs-2";
 import { Header } from "../../../components/Header";
-import { Chart as ChartJS, ArcElement, Tooltip, Legend } from "chart.js";
-import { Doughnut } from "react-chartjs-2";
+import SharedStats from "../components/Stats";
 
-ChartJS.register(ArcElement, Tooltip, Legend);
+ChartJS.register(
+  ArcElement,
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend
+);
 
 const cyans = [
   "rgba(22, 78, 99, alpha)",
@@ -24,10 +44,28 @@ const multi = [
   "rgba(15, 118, 110, alpha)",
   "rgba(67, 56, 202, alpha)",
   "rgba(185, 28, 28, alpha)",
-]
+];
 
 export const colourPicker = (palette, number, alpha) =>
   palette.slice(0, number).map((rgba) => rgba.replace("alpha", `${alpha}`));
+
+export const data3 = {
+  labels: ["January", "February", "March", "April", "May", "June", "July"],
+  datasets: [
+    {
+      label: "Dataset 1",
+      data: [256, 476, 611, 624, 785, 346, 567],
+      borderColor: colourPicker(cyans, 3, 1)[2],
+      backgroundColor: colourPicker(cyans, 3, 0.5)[2],
+    },
+    {
+      label: "Dataset 2",
+      data: [70, 672, 123, 142, 564, 726, 345],
+      borderColor: colourPicker(multi, 3, 1)[2],
+      backgroundColor: colourPicker(multi, 3, 0.5)[2],
+    },
+  ],
+};
 
 const data = {
   labels: ["900", "800", "700", "600", "500", "400"],
@@ -55,6 +93,34 @@ const data2 = {
   ],
 };
 
+const stats = [
+  {
+    name: "Total Customers",
+    stat: 71897,
+    previousStat: 70946,
+    change: 12,
+    changeType: "increase",
+  },
+  {
+    name: "Orders with >2 Items",
+    stat: 58.16,
+    previousStat: 56.14,
+    suffix: "%",
+    decimals: 2,
+    change: 2.02,
+    changeType: "increase",
+  },
+  {
+    name: "Percentage of Customers with the 'Tan' surname",
+    stat: 24.57,
+    previousStat: 28.62,
+    suffix: "%",
+    decimals: 2,
+    change: 4.05,
+    changeType: "decrease",
+  },
+];
+
 export const ManageDashboard = () => {
   return (
     <>
@@ -72,6 +138,14 @@ export const ManageDashboard = () => {
           <div className="rounded-lg bg-white overflow-hidden shadow m-4 p-6">
             <h3 className="text-lg font-medium">SORA</h3>
             <Doughnut data={data} />
+          </div>
+          <div className="rounded-lg bg-white overflow-hidden shadow m-4 p-6">
+            <h3 className="text-lg font-medium">Customer Figures</h3>
+            <SharedStats stats={stats} />
+          </div>
+          <div className="rounded-lg bg-white overflow-hidden shadow m-4 p-6 md:col-span-2">
+            <h3 className="text-lg font-medium">Sales</h3>
+            <Line data={data3} />
           </div>
         </div>
       </div>

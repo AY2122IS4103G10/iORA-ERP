1989cbe389681286ca0af31085d56afb9db6daa8
Remus <remuskwan23@gmail.com>
2022-03-31 19:33:17 +0800

    Bug fixes and Visual changes

--- ecommerce/src/views/components/Pagination/SimplePaginator/index.js
+++ ecommerce/src/views/components/Pagination/SimplePaginator/index.js
@@ -9,18 +9,18 @@ export const SimplePaginator = ({
   currentPage,
 }) => {
   const pages = [];
-  
+
   for (let i = 1; i <= Math.ceil(totalItems / itemsPerPage); i++) {
     pages.push(i);
   }
-  
+
   return (
     <nav
       aria-label="Pagination"
       className="max-w-7xl mx-auto px-4 mt-6 flex justify-between text-sm font-medium text-gray-700 sm:px-6 lg:px-8"
     >
       <div className="min-w-0 flex-1">
-        {currentPage !== pages[0] && (
+        {Boolean(pages.length) && currentPage !== pages[0] && (
           <button
             className="inline-flex items-center px-4 h-10 border border-gray-300 rounded-md bg-white hover:bg-gray-100 focus:outline-none focus:border-indigo-600 focus:ring-2 focus:ring-offset-1 focus:ring-offset-indigo-600 focus:ring-indigo-600 focus:ring-opacity-25"
             onClick={paginateFront}
@@ -31,8 +31,9 @@ export const SimplePaginator = ({
       </div>
       <div className="hidden space-x-2 sm:flex">
         {/* Current: "border-indigo-600 ring-1 ring-indigo-600", Default: "border-gray-300" */}
-        {pages.slice(0, 3).map((page) => (
+        {pages.map((page) => (
           <button
+            key={page}
             className={classNames(
               currentPage === page
                 ? "border-indigo-600 ring-1 ring-indigo-600"
@@ -65,7 +66,7 @@ export const SimplePaginator = ({
           ))} */}
       </div>
       <div className="min-w-0 flex-1 flex justify-end">
-        {currentPage !== pages[pages.length - 1] && (
+        {Boolean(pages.length) && currentPage !== pages[pages.length - 1] && (
           <button
             className="inline-flex items-center px-4 h-10 border border-gray-300 rounded-md bg-white hover:bg-gray-100 focus:outline-none focus:border-indigo-600 focus:ring-2 focus:ring-offset-1 focus:ring-offset-indigo-600 focus:ring-indigo-600 focus:ring-opacity-25"
             onClick={paginateBack}

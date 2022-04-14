3caa3ff895aae6bc0ad0d481bbd67a4657008991
hongpeigoh <57928283+hongpeigoh@users.noreply.github.com>
2022-04-14 21:49:42 +0800

    cancelling doesnt submit button

--- react-ui/src/views/containers/Auth/Login/index.js
+++ react-ui/src/views/containers/Auth/Login/index.js
@@ -1,9 +1,9 @@
-import {useState} from "react";
-import {useDispatch} from "react-redux";
-import {useNavigate} from "react-router-dom";
-import {useToasts} from "react-toast-notifications";
-import {authApi} from "../../../../environments/Api";
-import {loginJwt, postLoginJwt} from "../../../../stores/slices/userSlice";
+import { useState } from "react";
+import { useDispatch } from "react-redux";
+import { useNavigate } from "react-router-dom";
+import { useToasts } from "react-toast-notifications";
+import { authApi } from "../../../../environments/Api";
+import { loginJwt, postLoginJwt } from "../../../../stores/slices/userSlice";
 
 export function Login() {
   const dispatch = useDispatch();
@@ -13,12 +13,14 @@ export function Login() {
   const [email, setEmail] = useState("");
   const [resetPassword, setResetPassword] = useState(false);
   const [processing, setProcessing] = useState(false);
-  const {addToast} = useToasts();
+  const { addToast } = useToasts();
 
   const handleLogin = async (e) => {
     e.preventDefault();
     try {
-      const response = dispatch(loginJwt({username: username, password: password}));
+      const response = dispatch(
+        loginJwt({ username: username, password: password })
+      );
       const data = await response.unwrap();
       localStorage.setItem("accessToken", data.accessToken);
       localStorage.setItem("refreshToken", data.refreshToken);
@@ -46,11 +48,14 @@ export function Login() {
     e.preventDefault();
     setProcessing(true);
     try {
-      const {data} = await authApi.resetPassword({email, username});
-      addToast(`Password was successfuly reset. ${data} Check your e-mail for temporary password`, {
-        appearance: "success",
-        autoDismiss: true,
-      });
+      const { data } = await authApi.resetPassword({ email, username });
+      addToast(
+        `Password was successfuly reset. ${data} Check your e-mail for temporary password`,
+        {
+          appearance: "success",
+          autoDismiss: true,
+        }
+      );
       setResetPassword(false);
     } catch (err) {
       setTimeout(() => {
@@ -67,16 +72,28 @@ export function Login() {
     <>
       <div className=" min-h-full flex flex-col justify-center py-12 sm:px-6 lg:px-8">
         <div className="sm:mx-auto sm:w-full sm:max-w-md">
-          <img className="mx-auto h-12 w-auto" src="android-chrome-512x512.png" alt="iORA" />
-          <h2 className="mt-6 text-center text-3xl font-extrabold text-gray-900">iORA ERP</h2>
+          <img
+            className="mx-auto h-12 w-auto"
+            src="android-chrome-512x512.png"
+            alt="iORA"
+          />
+          <h2 className="mt-6 text-center text-3xl font-extrabold text-gray-900">
+            iORA ERP
+          </h2>
           <p className="mt-2 text-center text-sm text-gray-600"></p>
         </div>
 
         <div className="mt-8 sm:mx-auto sm:w-full sm:max-w-md">
           <div className="bg-white border py-8 px-4 shadow sm:rounded-lg sm:px-10">
-            <form className="space-y-6" onSubmit={resetPassword ? handleReset : handleLogin}>
+            <form
+              className="space-y-6"
+              onSubmit={resetPassword ? handleReset : handleLogin}
+            >
               <div>
-                <label htmlFor="username" className="block text-sm font-medium text-gray-700">
+                <label
+                  htmlFor="username"
+                  className="block text-sm font-medium text-gray-700"
+                >
                   Username
                 </label>
                 <div className="mt-1">
@@ -95,7 +112,10 @@ export function Login() {
               {resetPassword ? (
                 <>
                   <div>
-                    <label htmlFor="email" className="block text-sm font-medium text-gray-700">
+                    <label
+                      htmlFor="email"
+                      className="block text-sm font-medium text-gray-700"
+                    >
                       Enter your recovery and work e-mail for verification.
                     </label>
                     <div className="mt-1">
@@ -112,15 +132,20 @@ export function Login() {
                   </div>
                   <div className="flex sm:grid sm:grid-cols-2 gap-2">
                     <button
-                      onClick={() => setResetPassword(false)}
+                      onClick={(e) => {
+                        e.preventDefault();
+                        setResetPassword(false);
+                      }}
                       type="button"
-                      className="inline-flex justify-center px-3 py-2 border border-gray-300 shadow-sm text-sm leading-4 font-medium rounded-md text-gray-700 bg-white hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-cyan-500">
+                      className="inline-flex justify-center px-3 py-2 border border-gray-300 shadow-sm text-sm leading-4 font-medium rounded-md text-gray-700 bg-white hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-cyan-500"
+                    >
                       Cancel
                     </button>
                     <button
                       type="submit"
                       className="w-full flex justify-center px-3 py-2 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-cyan-600 hover:bg-cyan-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-cyan-500"
-                      disabled={processing}>
+                      disabled={processing}
+                    >
                       {processing ? (
                         <p className="animate-bounce">Processing...</p>
                       ) : (
@@ -132,7 +157,10 @@ export function Login() {
               ) : (
                 <>
                   <div>
-                    <label htmlFor="password" className="block text-sm font-medium text-gray-700">
+                    <label
+                      htmlFor="password"
+                      className="block text-sm font-medium text-gray-700"
+                    >
                       Password
                     </label>
                     <div className="mt-1">
@@ -150,7 +178,8 @@ export function Login() {
                   <div>
                     <button
                       type="submit"
-                      className="w-full flex justify-center py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-cyan-600 hover:bg-cyan-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-cyan-500">
+                      className="w-full flex justify-center py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-cyan-600 hover:bg-cyan-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-cyan-500"
+                    >
                       Login
                     </button>
                   </div>
@@ -160,7 +189,8 @@ export function Login() {
                       <button
                         type="a"
                         onClick={() => setResetPassword(true)}
-                        className="font-medium text-cyan-600 hover:text-cyan-500">
+                        className="font-medium text-cyan-600 hover:text-cyan-500"
+                      >
                         Forgot your password?
                       </button>
                     </div>

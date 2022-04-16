a031b51325e8a571893a887e743a26543f8b8b42
delvenwpy <72071732+wongpengyu@users.noreply.github.com>
2022-04-16 09:53:39 +0800

    Update AdminServiceImpl.java

--- erp/src/main/java/com/iora/erp/service/AdminServiceImpl.java
+++ erp/src/main/java/com/iora/erp/service/AdminServiceImpl.java
@@ -457,7 +457,7 @@ public class AdminServiceImpl implements AdminService {
         c.getVendors().remove(v);
     }
 
-*    @Override
+    @Override
     public Company editCompany(Company company) throws CompanyException {
         Company old = getCompanyById(company.getId());
 

b4776e825c945614ab2d5784bd45cef25b0d5777
Remus <36059985+remuskwan@users.noreply.github.com>
2022-07-15 18:42:00 +0800

    Revert "Delete config.js"
    This reverts commit 781c220a6c8ad1937d1b6a5b5fe7471f98bacc2e.

---
+++ react-ui/src/config.js
@@ -0,0 +1,14 @@
+export const IMGBB_SECRET = "72971cfd7358a13d6543e5aa7e187e5e"
+
+//Amazon S3
+const S3_BUCKET = "iora-erp"
+const REGION = "ap-southeast-1"
+const ACCESS_KEY = "AKIARTYBCSQYBZ2IOWWC"
+const SECRET_ACCESS_KEY = "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"
+
+export const awsConfig = {
+  bucketName: S3_BUCKET,
+  region: REGION,
+  accessKeyId: ACCESS_KEY,
+  secretAccessKey: SECRET_ACCESS_KEY,
+};
